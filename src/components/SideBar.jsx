f366c42266c9b9097a53ff461266a7d77eb6a722
luisc68 <j.cerna@unadeca.net>
2023-08-04 13:36:09 -0600

    "Add button logout"

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,6 +4,7 @@ import foto from '../assets/2.svg';
 import home from '../assets/hom3.svg';
 import openbooks from '../assets/openbook.svg';
 import stud from '../assets/graduated.svg';
+import logout from '../assets/log-out.svg';
 
 function SideBar() {
   return (
@@ -12,6 +13,9 @@ function SideBar() {
       <SidebarLink text="Home" img1={home} href="/" />
       <SidebarLink text="Books" img1={openbooks} href="/books" />
       <SidebarLink text="Students" img1={stud} href="/students" />
+      <div className="iconlog">
+        <SidebarLink text="Log Out" img1={logout} href="/Login" />
+      </div>
     </div>
   );
 }
